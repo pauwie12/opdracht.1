d8981fa10d2175b32eaf600c6a5535900a70fa6c
pauwie12 <100357855+pauwie12@users.noreply.github.com>
2022-03-06 19:28:19 +0100

    Add files via upload

---
+++ opdracht.1.1.py
@@ -0,0 +1,23 @@
+
+address=range (0-255)
+
+
+hostname = input ("geef uw naam ") 
+ip = input("geef uw ip address ")
+if  ip !=  (address, address ,address,address ):
+  print('Invalid IP Address')
+  ip = input ('kies een bestaand ip address')
+OS  = input ("kies uw operating system: Windows, linux, macOS ")
+if OS not in {"Linux", "Windows", "macOS" }: #kijkt of een operating system geldig is
+ print ("dit is geen geldig operating system ") 
+ OS =input ("kies een bestaand operating system ")
+print ("uw naam is " + hostname)
+
+print ("uw ip address is  " + ip)
+
+print ("uw operating system is "+ OS )
+
+savedata= input ("moeten uw gegevens opgeslagen worden ja/nee ")
+if savedata = ja :
+ 
+ 
